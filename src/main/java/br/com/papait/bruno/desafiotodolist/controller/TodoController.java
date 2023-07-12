34ae8060439d27a5c3a08c914aa32a0a24ad6156
brunopapait <brunohpapait@gmail.com>
2023-07-11 22:56:27 -0300

    feat: config annotations with methods http and transactions

--- src/main/java/br/com/papait/bruno/desafiotodolist/controller/TodoController.java
+++ src/main/java/br/com/papait/bruno/desafiotodolist/controller/TodoController.java
@@ -3,10 +3,9 @@ package br.com.papait.bruno.desafiotodolist.controller;
 
 import br.com.papait.bruno.desafiotodolist.entity.Todo;
 import br.com.papait.bruno.desafiotodolist.service.TodoService;
-import org.springframework.data.domain.Sort;
+import jakarta.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,19 +19,26 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    public ResponseEntity<List<Todo>> create(Todo todo) {
+    @PostMapping
+    @Transactional
+    public ResponseEntity<List<Todo>> create(@RequestBody Todo todo) {
         return ResponseEntity.ok(this.todoService.create(todo));
     }
 
+    @GetMapping
     public ResponseEntity<List<Todo>> findAll() {
         return ResponseEntity.ok(this.todoService.findAll());
     }
 
-    public ResponseEntity<List<Todo>> update(Todo todo) {
+    @PutMapping
+    @Transactional
+    public ResponseEntity<List<Todo>> update(@RequestBody Todo todo) {
         return ResponseEntity.ok(this.todoService.update(todo));
     }
 
-    public ResponseEntity<List<Todo>> delete(Long id) {
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<List<Todo>> delete(@PathVariable Long id) {
         return ResponseEntity.ok(this.todoService.delete(id));
     }
 }
